5290d34c690401db6868355bd9de532696d9945b
Mamogapi <direkogabi@gmail.com>
2023-02-16 09:16:08 -0800

    hellow world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int q;
+	long int w;
+	long long int e;
+	char r;
+	float t;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(r));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(q));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(w));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeo(e));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(t));
+	return (0);
+}
