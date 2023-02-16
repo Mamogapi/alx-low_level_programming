809fd88976ed49d31146b03e85762d90c90f118f
Mamogapi <direkogabi@gmail.com>
2023-02-16 08:52:29 -0800

    print the text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle" 
- * Returns 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
- 
- int main(void)
-	
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
- 	return (0);
+	return (0);
 }
