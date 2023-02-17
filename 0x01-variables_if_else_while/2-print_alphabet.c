3fc5d12ba48127c640d377566fc8caf5ba5f9320
Mamogapi <direkogabi@gmail.com>
2023-02-17 07:23:19 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Sucess)
+*/
+ 
+int main(void)
+{
+	char alph;
+
+ 	do {
+	alph = 'a';
+	putchar(alph);
+	alph++;
+	
+	} while (alph !>  'z');
+	putchar(alph);
+	return (0);
+}
+
