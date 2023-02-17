9940db7fbd14aecf4f0c00d08505e4b71f524fb5
Mamogapi <direkogabi@gmail.com>
2023-02-17 11:41:22 -0800

    print number

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,14 @@
 * Return: Always 0 (Succes)
 */
 
-int main (void)
+int main(void)
 {
 	int x;
 
 	for (x = 0 ; x < 10; x++)
 	{
 		putchar(x + '0');
-		if(x < 9)
+		if (x < 9)
 		{
 			putchar(',');
 			putchar(32);
