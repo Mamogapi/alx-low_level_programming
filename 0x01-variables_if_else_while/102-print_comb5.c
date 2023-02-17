d02be03a792ab4e81faec68c2008af110ec73131
Mamogapi <direkogabi@gmail.com>
2023-02-17 15:57:49 -0800

    print

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -24,7 +24,7 @@ int main(void)
                                 	putchar('0' + y);
 					putchar(32);
 				
-					if (!( x > 9 ))
+					if (!( x > 11 ))
 					{	putchar('0' + z);
                                         	putchar('0' + a);
 						putchar(',');
