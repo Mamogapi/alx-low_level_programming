c962baecdd9a44c8a598e1cd4df927528903f906
Mamogapi <direkogabi@gmail.com>
2023-02-21 03:33:19 -0800

    isalpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,7 +10,7 @@
 void print_alphabet(void)
 {
 	char alph[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-		'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u',
+		'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
 		'v', 'w', 'x', 'y', 'z' };
 
 	int i;
