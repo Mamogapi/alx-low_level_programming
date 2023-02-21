4f08986116429fc922ea2fa20e2e372d8afa3448
Mamogapi <direkogabi@gmail.com>
2023-02-20 23:27:35 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - function prints the alphabet, in lowercase
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char alph[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u',
+		'v', 'w', 'x', 'y', 'z' };
+
+	int i;
+
+	for (i = 0; i < 26 ; i++)
+	{
+		__putchar(alph[i]);
+	}
+	_putchar('\n');
+
+}
+
+/**
+ * main - call the function print_alphabet()
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+
+	print_alphabet();
+	return (0);
+}
