9c121b0b07a24672de4e35e4e40a64536fa1ffc1
Mamogapi <direkogabi@gmail.com>
2023-02-20 22:10:45 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  prints _putchar, followed by a new line
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	char messg[] = "_putchar";
+	int x;
+
+	for (x = 0; x < MAXSTRING; x++)
+	{
+		putchar(messg[x]);
+	}
+	putchar('\n');
+
+	return (0);
+}
