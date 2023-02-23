05b9a6e38fd46affe8a9c588503d70b81252f3cc
Mamogapi <direkogabi@gmail.com>
2023-02-23 09:40:14 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * more_numbers -  prints 10 times the numbers, from 0 to 14,
  * followed by a new line
@@ -15,7 +15,9 @@ void more_numbers(void)
 	{
 		for (x = 0; x <= 14; x++)
 		{	if(x >= 10)
-			_putchar((x / 10) + '0');
+			{
+				_putchar((x / 10) + '0');
+			}
 
 			_putchar((x % 10) + '0');
 		}
