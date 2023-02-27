df20fc93cc212a5ec51acfad016842f8d113d068
Mamogapi <direkogabi@gmail.com>
2023-02-27 09:33:54 -0800

    100c-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,28 +11,29 @@
 int _atoi(char *s)
 {
 	int x = 0;
-	int sign=1;
-	unsigned int number=0;
+	int sign = 1;
+	unsigned int number = 0;
 
 	while (s[x] != '\0')
-	{	
+	{
 
 		if ((s[x] < '0' || s[x] > '9') && (s[x] == '-'))
+
 		{
-			sign = sign * -1;
-		
+			sign = sign * -1;	
 		}
 		else if ((s[x] >= '0' && s[x] <= '9'))
+
 		{	
 			number = number * 10 + (s[x] - '0');
 			if ((s[x + 1] < '0' || s[x + 1] > '9'))
+
 			{	
 				break;
 			}
 		
 
 		}
-		
 		x++;
 	
 	
