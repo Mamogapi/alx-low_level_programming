655fec4104ea9afcbeb649d6ebb2ae86694122d3
Mamogapi <direkogabi@gmail.com>
2023-02-27 05:33:51 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string, starting with
+ * the first character, followed by a new line.
+ * @str: character variable
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int x, z;
+
+	z = strlen(str);
+	z--;
+
+	for (x = 0; x <= z; x++)
+	{
+		if ((x % 2) = 0)
+		{
+			_putchar(str[x]);
+		}
+	}
+
+	_putchar('\n');
+}
