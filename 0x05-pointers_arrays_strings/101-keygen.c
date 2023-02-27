c88858eff49b0f624d699b8b7851954b1011fd2e
Mamogapi <direkogabi@gmail.com>
2023-02-27 10:03:51 -0800

    101 -keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for the program
+ * 
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char str[100];
+
+	int x = 0, randnumber = 0, sum = 0;
+
+	srand (time(null));
+
+	for (x = 0; sum <= 2644; x++)
+
+	{	
+		randnumber = (rand() % 25) + 65;
+
+
+		str[x] = randnumber;
+		sum = sum + randnumber;
+
+	}
+
+	str[x++] = 2772 - sum;
+	str[x++] = '\0';
+
+	printf("%s\n", str);
+
+	return (0);
+}
