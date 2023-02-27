3297e0b8dec806a3a45febba8f6bd858efc6508a
Mamogapi <direkogabi@gmail.com>
2023-02-27 04:44:41 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,5 +9,19 @@
 
 void rev_string(char *s)
 {
-	strrev(s);
+	int a, b;
+	char temp;
+
+	a = 0;
+	b = 0;
+
+	b = strlen(s);
+
+	for (a; a < b; a++)
+	{
+		b--;
+		temp = s[a];
+		s[a] = s[b];
+		s[b] = temp;
+	}
 }
