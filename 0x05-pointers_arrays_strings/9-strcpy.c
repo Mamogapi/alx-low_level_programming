03180bc03293d00c13004b6abb101d9b0be2f547
Mamogapi <direkogabi@gmail.com>
2023-02-27 08:04:20 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src, including the
+ * terminating null byte (\0), to the buffer pointed to by dest.
+ * @dest: character valriable
+ * @src: character variable
+ * Return: 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcp(dest,src));
+}
