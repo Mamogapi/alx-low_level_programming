e5e6be87c017c17ba8c024d5cf34a564496487aa
Mamogapi <direkogabi@gmail.com>
2023-02-27 07:32:30 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_array - prints n elements of an array of integers, followed
+ * by a new line
+ * @a: integer value
+ * @n: integer value
+ */
+
+void print_array(int *a, int n)
+{
+	int x = 0;
+
+	for (x; x <= n; x++)
+	{
+		printf("%d", a[x]);
+		if (x != n)
+		printf(", ");
+	}
+
+}
