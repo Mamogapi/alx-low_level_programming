cd0021f4f4c313e1d5ab7afd1c992745409a482e
Mamogapi <direkogabi@gmail.com>
2023-02-27 02:33:53 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: character variable 
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
