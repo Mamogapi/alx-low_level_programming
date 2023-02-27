d826b96c6bf5a5140aa0b6ae61f386283634eb06
Mamogapi <direkogabi@gmail.com>
2023-02-27 06:34:54 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -21,7 +21,7 @@ void puts_half(char *str)
 
 	if (length_of_the_string % 2 == 1)
 	{
-		n = (length_of_the_string + 1) / 2;
+		n = (length_of_the_string - 1) / 2;
 		while (str[n] != '\0')
 		{
 			_putchar(str[n]);
