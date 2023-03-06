4ca96ee9591857be90f309f4225f800cbb5e99ae
Mamogapi <direkogabi@gmail.com>
2023-03-06 03:57:31 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,6 @@
  *  _strspn - gets the length of a prefix substring
  *  @s: string pointer variable
  *  @accect: string pointer variable
- *  
  *  Return: the number of bytes in the initial segment of s which
  *  consist only of bytes from accept
  */
@@ -16,7 +15,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (w = 0; s[w] != '\0'; w++)
 	{
-		for (z = 0; accept[z] =! '\0'; z++)
+		for (z = 0; accept[z] != '\0'; z++)
 		{
 			if (accept[z] == '\0')
 				return (w);
