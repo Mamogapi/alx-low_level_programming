e5ab940d810ab3c523f00866dc7dd24b89a9a88c
Mamogapi <direkogabi@gmail.com>
2023-03-06 04:30:53 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -15,15 +15,15 @@ char *_strpbrk(char *s, char *accept)
 {
 	int a, b;
 
-	for (a = 0; accept[a] != '\0'; a++)
+	for (a = 0; s[a] != '\0'; a++)
 	{
-		for (b = 0; s[b] != '\0'; b++)
+		for (b = 0; accept[b] != '\0'; b++)
 		{
 			if (s[a] == accept[b])
 			{
 				return (s + a);
 			}
-		}	
+		}
 	}
 
 		return (NULL);
