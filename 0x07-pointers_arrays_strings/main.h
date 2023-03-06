af4db38fc23c4305d6d8df89f331ba2abdd61470
Mamogapi <direkogabi@gmail.com>
2023-03-06 11:47:54 -0800

    101-crackme_password

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef main_h
+#define main_h
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -7,4 +10,7 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+int coincidence(char *a, char *b);
 
+#endif
