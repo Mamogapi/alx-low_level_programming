4625e64d2f0aa2a7ee52040ab537c2ea04c62f39
Mamogapi <direkogabi@gmail.com>
2023-03-14 14:31:06 -0700

    new

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,7 +11,7 @@
  */
 
 char *argstostr(int ac, char **av)
-{	
+{
 	int i, n, r = 0, l = 0;
 
 	char *str;
@@ -29,16 +29,17 @@ char *argstostr(int ac, char **av)
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
-	{	for (n = 0; av[i][n]; n++)
-		{	
+	{
+		for (n = 0; av[i][n]; n++)
+		{
 			str[r] = av[i][n];
 			r++;
 		}
 
-	if (str[r] == '\0')
-	{
-		str[r++] = '\n';
-	}
+		if (str[r] == '\0')
+		{
+			str[r++] = '\n';
+		}
 	}
 	return (str);
 
