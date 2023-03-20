396165eccf62f9eec96770f85b5f54b4dc289048
Mamogapi <direkogabi@gmail.com>
2023-03-20 08:30:32 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,8 +9,8 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)	
-		return;
+if (d)
+{
 	if (d->name == NULL)
 		printf("Name: (nil)");
 	else
@@ -22,4 +22,5 @@ void print_dog(struct dog *d)
 		printf("Owner: (nil)");
 	else
 		printf("Owner: %s\n", d->owner);
+}
 }
