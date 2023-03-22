91c3d36e08385668946670e2dad7fe52a9695ef7
Mamogapi <direkogabi@gmail.com>
2023-03-22 10:14:17 -0700

    new

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,29 +10,28 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int result;
-
+	int num1, num2;
+	char *op;
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2][1] != 0) || ((argv[2][0] != '+') && (argv[2][0] != '-')
-		&& (argv[2][0] != '*') && (argv[2][0] != '/') && (argv[2][0] != '%')))
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2][0] == '/' || (argv[2][0] == '%')) && atoi(argv[3]) == 0)
+	if ((*op == '/' && num2 == 0) ||(*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	result = get_op_func(argv[2]) (atoi(argv[1]), atoi(argv[3]));
-	printf("%d\n", result);
-
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
