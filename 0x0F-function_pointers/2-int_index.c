e29d7c3d6f392be95a07946e789ff25d4175c844
Mamogapi <direkogabi@gmail.com>
2023-03-22 03:28:28 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,12 +15,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int x;
 
-	if (size <= 0)
+	if (size <= 0 || cmp == NULL ||array == NULL)
 		return (-1);
 
 	for (x = 0; x < size; x++)
 	{
-		if((*cmp(array[x] == x)
+		if((*cmp(array[x])
 			return (x);
 	}
 	return (-1);
