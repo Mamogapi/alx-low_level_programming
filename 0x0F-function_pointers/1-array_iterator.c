01d15d50db91ac7d9a3f3f0310e715cac1d1725b
Mamogapi <direkogabi@gmail.com>
2023-03-22 03:25:26 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,6 +15,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x;
 
-	for (x = 0; x < size; x++)
-		(action)(array[x]);
+	if (array && action != NULL)
+	{
+		for (x = 0; x < size; x++)
+			(action)(array[x]);
+	}
 }
