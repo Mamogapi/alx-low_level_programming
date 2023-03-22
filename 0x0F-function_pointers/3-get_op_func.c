c05f7d4d5ce724cc037b7cea6282e6c4a3a77738
Mamogapi <direkogabi@gmail.com>
2023-03-22 09:17:15 -0700

    3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -25,12 +25,8 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (ops[i].op)
-	{
-		if (*(ops[i].op == *s)
-			return (ops[i].f);
-
+	while (ops[i].op != NULL && *(ops[i].op != *s)
 		i++;
-	}
-	return (0);
+
+	return (ops[i].f);
 }
