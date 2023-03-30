3e454395ff2efe728ced0e2a6b18942eac63240b
Mamogapi <direkogabi@gmail.com>
2023-03-30 04:48:11 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+int cal_len(const char *str);
+
+/**
+ * add_node - function adds a new node at the beginning of a list_t list
+ * @head: linker double pointer
+ * @str: pointer char variable
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+
+	temp = (list_t *) malloc(sizeof(list_t));
+
+	if (temp == NULL)
+		return (NULL);
+	temp->str = strdup(str);
+	temp->len = cal_len(str);
+	temp->next = *head;
+	*head = temp;
+	return (temp);
+}
+
+/**
+ * cal_len - fucntion calculate the length of string
+ * str: pointer string variable
+ * Return: length of string
+ */
+
+int cal_len(const char *str)
+{
+	int x = 0;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+int cal_len(const char *str);
+
+/**
+ * add_node_end - function adds a new node at the end of a list_t list
+ * @head: linker double pointer
+ * @str: pointer string
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t temp;
+
+	new = (list_t *)malloc(sizeof(list_t));
+
+	if (new == NULL || str == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = cal_len(str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new;
+	return (*new);
+}
+
+/**
+ * cal_len - function calculate the length of string
+ * @str: pointer string
+ * Return: the length of string
+ */
+
+int cal_len(const char *str)
+{
+	int x = 0;
+
+	while (str[x] != '\0')
+		x++;
+	return (x);
+}
