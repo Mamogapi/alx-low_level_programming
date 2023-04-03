811f675b0cf122e93d41fad2b357f823eae0c7e1
Mamogapi <direkogabi@gmail.com>
2023-04-03 10:40:54 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -10,20 +10,21 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *node1, *node2;
 
-	node1 = head;
-	node2 = head;
+	*node1 = head;
+	*node2 = head;
 
-	if (head == NULL)
+	if (!head)
 		return (NULL);
 
 	while (node1 && node2 && node2->next)
 	{
 		node2 = node2->next->next;
-		node1 = slow->next;
+		node1 = node1->next;
 
-		if (node1 == node2)
+		if (node2 == node1)
 		{
 			node1 = head;
+
 			while (node1 != node2)
 			{
 				node1 = node1->next;
