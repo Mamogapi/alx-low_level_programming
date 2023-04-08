090b04f65fffdd91215ed7bdd54c0b6a5640604f
Mamogapi <direkogabi@gmail.com>
2023-04-08 07:45:44 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,15 +12,14 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0, y = 1;
 	int x, z;
 
-	while (b != NULL)
-		z++;
-
 	if (b == NULL)
 		return (0);
-	for (x = 0; x <= z; x++)
+
+	for (x = 0; b[x] = '\0'; x++)
 	{
 		if (b[x] != '0' && b[x] != '1')
 			return (0);
+
 		num <<= 1;
 
 		if (b[x] & 1)
