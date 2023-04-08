972ccb034edae6165d03ca1f5431cf3af83071d1
Mamogapi <direkogabi@gmail.com>
2023-04-08 06:06:07 -0700

    101-password

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,16 +9,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int x = 0;
+	unsigned int num = 0, y = 1;
+	int x = 0, z;
 
+	while (b != NULL)
+		z++;
 	if (b == NULL)
 		return (0);
-	while (b[x] == '0' || b[x] == '1')
+	for (x = z - 1; c >= 0; x--)
 	{
-		num <<= 1;
-		num += b[x] - '0';
-		x++;
+		if (b[x] != '0' && b[x] != '1')
+			return (0);
+		if (b[x] == '1')
+			num += y;
+		y *= 2;
 	}
 	return (num);
 }
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - function that returns the number of bits you would need to
+ * flip to get from one number to another.
+ * @n: unsigned long int
+ * @m: unsigned long int
+ * Return: the number of bits you would need to flip to get from one
+ * number to another.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0;
+	unsigned int num = n ^ m;
+
+	while (num > 0)
+	{
+		count++;
+		num &= (num - 1);
+	}
+	return count;	
+}
